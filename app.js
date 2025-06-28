5423c8c5c6884249e693a2c3c12e5e9171d7a360
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-06-28 13:14:21 +0530

    Changes

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 "use strict";
-var a = 12;
-var b = 4;
-let c = "sushant";
-let d = true;
-const r = 13;
-let arr = [1, 2, 3, 4, 5, 'harsh'];
-const obj = {
-    name: "sushant",
-    age: 23,
-};
+function abcd(obj) {
+}
+abcd({ name: "sushant", age: 23 });
--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
-var a = 12;
-var b = 4;
-let c = "sushant";
-let d = true;
-const r = 13
-let arr = [1, 2, 3, 4, 5, 'harsh'];
-const obj = {
-    name: "sushant",
-    age : 23,
+// var a = 12;
+// var b = 4;
+// let c = "sushant";
+// let d = true;
+// const r = 13
+// let arr = [1, 2, 3, 4, 5, 'harsh'];
+// const obj = {
+//     name: "sushant",
+//     age : 23,
+// }
+interface Person {
+    name: string;
+    age: number;
 }
+
+function abcd(obj : Person) {
+}
+
+abcd({name : "sushant", age: 23});
